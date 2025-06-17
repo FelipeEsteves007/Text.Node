1abd0f04494b2da32b74545feca90cd8a5beef22
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-06-17 16:43:22 -0300

    captura palavras

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const link = caminhoArquivo[2]
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
     console.log(texto)
+    verificaPalavrasDuplicadas(texto) 
 })
 
 function verificaPalavrasDuplicadas (texto) {
     const listaPalavras = texto.split(' ') 
     const resultado = {}
 
-    listaPalavras.array.forEach(palavra => {
+    listaPalavras.forEach(palavra => { 
         resultado[palavra] = (resultado[palavra] || 0) + 1
     });
-    console.log(resultado);
-    
+    console.log(resultado)
 }
