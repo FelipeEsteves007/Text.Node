99409e7dc65fb43511523f6f71d60d627a6e97ff
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-06-26 10:40:07 -0300

    add

--- logic.js
+++ logic.js
@@ -1,10 +1,10 @@
-function processarNumeros (numeros, callback) {
-    for (let i = 0; i < numeros.length; i++){
-        callback(numeros[i])
+function processarNumeros (arr, callback){
+    for (let i = 0; i < arr.length; i++){
+        callback(arr[i])
     }
 }
 
 processarNumeros([10,20,30], (numero) => {
-    console.log(`O dobro de ${numero} é ${numero * 2}`);
+    console.log(`o dobro de ${numero} é ${numero * 2}`);
     
 })
