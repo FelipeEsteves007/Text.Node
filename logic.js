6d0df5dd89b867aa49311486bd24e1341b5e2ebe
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-06-30 11:37:45 -0300

    add

--- logic.js
+++ logic.js
@@ -9,10 +9,10 @@ function maiorMenor (arr){
     let menor = arr[0]
 
     for (let i = 0; i < arr.length; i++){
-        if (maior > arr[i]){
+        if ( arr[i] > maior){
             maior = arr[i] 
         }
-        if (menor < arr[i]){
+        if (arr[i] < menor ){
             menor = arr[i] 
         }
     }
