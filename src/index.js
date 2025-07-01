d8d26196f2c650b696736516c8788c975b491f53
Felipe Esteves <felipeestevesdepaula@Gmail.com>
2025-07-01 13:29:39 -0300

    add

--- src/index.js
+++ src/index.js
@@ -4,5 +4,40 @@ const caminhoArquivo = process.argv; // pega o nome do arquivo digitado no termi
 const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  console.log(texto); // mostra o conteúdo
+  quebraEmParagragos(texto)
+  //verificaPalavrasDuplicas(texto)
 });
+
+// criar um arr de palavras
+// contador se a palavra é duplicada
+// objeto com o resultado
+{/*{
+  'web' = 5;
+}
+*/}
+
+function quebraEmParagragos (texto){
+  const paragrafos = texto.toLowerCase().split('\n')
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return []
+    return verificaPalavrasDuplicas(paragrafo)
+  })
+  console.log(contagem);
+}
+
+function limpaPalavras (palavra){
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '') // expressão regular (padrões em texto)
+}
+
+function verificaPalavrasDuplicas (texto){
+  const listaPalavras = texto.split(' ')
+  const resultado = {}
+  //objeto[propriedade] = valor
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3){
+      const palavraLimpar = limpaPalavras(palavra)
+    resultado[palavraLimpar] = (resultado[palavraLimpar] || 0) + 1
+    }
+  });
+  return resultado
+}
